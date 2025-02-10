8d178b21700410635fa3dffba1220ab67154e081
Space <ashrayvarughese@gmail.com>
2025-02-10 18:49:11 +0530

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,7 +1,10 @@
 :: this is a comment
 
-@echo off :: this removes the dir thinge
+@echo off 
+:: this removes the dir thinge
 
-echo "Hello world!" :: This outputs Hello World out in the command prompt
+echo "Hello world!" 
+:: This outputs Hello World out in the command prompt
 
-pause :: this make you see the window, if not there you will only see window for a split second
+pause 
+:: this make you see the window, if not there you will only see window for a split second
